b7b01addfb3d118f4609446c74ae174391556779
VloRyan <florian685@gmail.com>
2025-08-19 15:14:36 +0200

    feat: usePage with defaults

--- hooks/UsePage.ts
+++ hooks/UsePage.ts
@@ -2,6 +2,13 @@ import { Page } from "@vloryan/ts-jsonapi-form/jsonapi/JsonApi";
 import { extractPage } from "@vloryan/ts-jsonapi-form/jsonapi/Request";
 import { useSearch } from "wouter";
 
-export const usePage = () => {
-  return extractPage(useSearch()) || ({ limit: 25, offset: 0 } satisfies Page);
+export const usePage = (defaults?: Page) => {
+  if (!defaults) {
+    defaults = { limit: 25, offset: 0 } satisfies Page;
+  }
+  const p = extractPage(useSearch());
+  return {
+    limit: p?.limit ?? defaults.limit,
+    offset: p?.offset ?? defaults.offset,
+  } satisfies Page;
 };
