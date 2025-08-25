a40033ef9fd8ba3d7f96028753b5987abd4f19c9
VloRyan <florian685@gmail.com>
2025-08-25 10:30:40 +0200

    fix: naming

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,4 +2,4 @@ export * from "./UseAlert";
 export * from "./UseFilter";
 export * from "./UsePage";
 export * from "./UseResource";
-export * from "./UseDocumentFormForm.ts";
+export * from "./UseDocumentForm.ts";
